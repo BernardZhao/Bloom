f6d1f38177e260e02a2b70c7d19733e82061f30d
Vasanth Sadhasivan <vasanthsadhasivan@gmail.com>
2016-11-05 21:45:24 -0700

    AY PUSH ALL GOOD??

--- Ups/app/src/main/java/com/example/wad/ups/NotificationsListenerService.java
+++ Ups/app/src/main/java/com/example/wad/ups/NotificationsListenerService.java
@@ -1,6 +1,21 @@
 package com.example.wad.ups;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.gcm.GcmListenerService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by berna on 11/5/2016.
@@ -8,4 +23,45 @@ import com.google.android.gms.gcm.GcmListenerService;
 
 public class NotificationsListenerService extends GcmListenerService {
 
+    public static final int NOTIFICATION_ID = 1;
+    /**
+     * Called when message is received.
+     *
+     * @param from SenderID of the sender.
+     * @param data Data bundle containing message data as key/value pairs.
+     *             For Set of keys use data.keySet().
+     */
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        sendNotification(data.getBundle("notification").getString("title"), data.getBundle("notification").getString("body"), data.getBundle("notification").getString("icon") );
+    }
+
+    /**
+     *  Put the message into a notification and post it.
+     *  This is just one simple example of what you might choose to do with a GCM message.
+     *
+     * @param message The alert message to be posted.
+     */
+    private void sendNotification(String message, String message2, String icon) {
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent contentIntent =
+                PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        // Notifications using both a large and a small icon (which yours should!) need the large
+        // icon as a bitmap. So we need to create that here from the resource ID, and pass the
+        // object along in our notification builder. Generally, you want to use the app icon as the
+        // small icon, so that users understand what app is triggering this notification.
+        Bitmap largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_cloud_white_48dp);
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_cloud_white_48dp)
+                        .setLargeIcon(largeIcon)
+                        .setContentTitle(message)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                        .setContentText(message2)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
 }
