4f49266c28d3d569d3f82f6dd5b64f3a49b727ff
Sam <arca6277@eduhsd.k12.ca.us>
2016-11-09 08:30:51 -0800

    basic notifications

--- Ups/app/src/main/java/com/example/wad/ups/Form.java
+++ Ups/app/src/main/java/com/example/wad/ups/Form.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.app.NotificationCompat;
+import android.app.NotificationManager;
 
 public class Form extends AppCompatActivity {
 
@@ -16,5 +18,13 @@ public class Form extends AppCompatActivity {
     public void add(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_close_light)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
